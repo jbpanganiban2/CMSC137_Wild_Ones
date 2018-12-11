754777d1ccb7510affe22a6be53d8dfdcee3282b
aaporca <aaporca@up.edu.ph>
2018-12-11 22:29:07 +0800

    fixed static

--- proto/Lobby.java
+++ proto/Lobby.java
@@ -244,10 +244,10 @@ public class Lobby extends JPanel{
           }
      }
 
-     private static Player getUser(Player[] online){
+     private Player getUser(Player[] online){
           for(Player p : online){
                if(p == null)continue;
-               if(user.getName().equals(p.getName()))return p;
+               if(this.user.getName().equals(p.getName()))return p;
           }return user;
      }
 
@@ -412,7 +412,7 @@ public class Lobby extends JPanel{
           return btn;
      }
 
-     static class SetChoice implements ActionListener{
+     class SetChoice implements ActionListener{
 
           int value = 0;
           Lobby l;
